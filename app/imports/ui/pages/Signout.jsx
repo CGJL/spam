60c134acaa8d464891ed9109b6a2ed8d869927ed
Jessica Ocampo <jnocampo@hawaii.edu>
2021-06-20 23:18:23 -1000

    fixing syntax from password to passwords

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Header } from 'semantic-ui-react';
-import { EncryptionKey } from '../../api/encryption/EncryptionKey'
+import { EncryptionKey } from '../../api/encryption/EncryptionKey';
 
 /** After the user clicks the "Signout" link in the NavBar, log them out and display this page. */
 export default class Signout extends React.Component {
   render() {
     // Clear local MongoDB collection holding the user's encryption key
     EncryptionKey.remove({});
-    
     Meteor.logout();
     return (
       <Header id="signout-page" as="h2" textAlign="center">
